c7af55da1bfeb4d7f571dc55874ff0ee60fd2c14
Ugo Capelle <capelleu@icloud.com>
2024-11-29 12:05:17 +0100

    log pour deploy test

--- kasa/src/Router.js
+++ kasa/src/Router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
@@ -6,6 +6,7 @@ import LogementDetail from './pages/LogementPage';
 import Header from './components/Header';
 
 function AppRouter() {
+  console.log('App is rendering');
   return (
     <Router>
       <Header />
